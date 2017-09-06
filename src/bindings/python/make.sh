c0c100e28f906d0735ecf0b6308349e72e0a73df
Coastinger <luedders.h@gmail.com>
2017-09-06 15:53:15 +0200

    Update make.sh
    changed paths and removed test

--- src/bindings/python/make.sh
+++ src/bindings/python/make.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 
-OPENALPR_INCLUDE_DIR=/storage/projects/alpr/src/openalpr/
-OPENALPR_LIB_DIR=/storage/projects/alpr/src/build/openalpr/
+OPENALPR_INCLUDE_DIR=/opt/openalpr/src/openalpr/
+OPENALPR_LIB_DIR=/opt/openalpr/src/build/openalpr/
 
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:.:${OPENALPR_LIB_DIR}
 
 g++ -Wall -L${OPENALPR_LIB_DIR} -I${OPENALPR_INCLUDE_DIR} -shared -fPIC -o libopenalprpy.so openalprpy.cpp -lopenalpr
-
-python test.py
